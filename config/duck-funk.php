1d384171c3734cb6a443ab60381e73a2a820ba29
David Torralbo <david@dtp.cat>
2019-10-10 18:36:34 +0200

    Add duck funk config

---
+++ config/duck-funk.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * Feel free to modify Duck Funk CMS parameters here.
+ */
+return [
+
+    /*
+     * The route the CMS core will get to build the website.
+     */
+    'route' => '/',
+
+    /*
+     * The view template the CMS core will get to build the website.
+     *
+     * As default, Duck Funk Core will get fuse template.
+     */
+
+    'template' => 'fuse',
+];
